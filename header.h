018ec48e3c835d91ae8e4646b5f5a5d548131022
zack242 <zack242@users.noreply.github.com>
2020-04-26 20:18:49 +0200

    Mise en place du Menu V1
    Menu J1 avec lecture des fichiers

--- header.h
+++ header.h
@@ -1,5 +1,16 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+
+
+///Proto
+void menu();
+
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
+#include "header.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-     cout << "bienvenue au projet piscine" << endl;
+    cout << "--Projet Theorie des graphes-- \n" << endl;
+    while(!0)
+    menu();
+
     return 0;
+}
+
+void menu()
+{
+int choix ;
+std::string fichier_topo;
+std::string fichier_ponde;
+
+std::cout<<"            Menu \n\n";
+
+std::cout<<"1/Chargement d'un Graphe. \n";
+std::cout<<"2/Chagement du systeme de ponderation. \n";
+std::cout<<"3/Calculer, afficher et Sauvegarder les differents indices de centralite.\n";
+std::cout<<"4/Tester la vulnerabilite. \n";
+std::cout<<"99/Exit\n\n ";
+std::cout<<"Votre choix : " ;
+std::cin>>choix ;
+std::cout<<"\n";
+
+switch (choix)
+{
+
+case 1 :
+
+    std::cout<<"- Chargement d'un Graphe.\n";
+
+    std::cout<<"Nom du fichier de topologie : ";
+    std::cin>>fichier_topo;
+    std::cout<<"Nom du fichier de ponderations : ";
+    std::cin>>fichier_ponde;
+
+    std::cout<<"\n Chargement du graphe avec "<<fichier_topo << "et" <<fichier_ponde<< " \n\n" ;
+
+break;
+
+case 99 :
+    exit(0);
+
+
+default :
+    std::cout<<"Erreur \n";
+
+}
+
+
+
 }

f005f4360d62441480111b6409144268bb02c8d8
zack242 <zack242@users.noreply.github.com>
2020-04-27 01:07:55 +0200

    Chargement d'un graphe
    Chargment d'un graphe depuis 2 fichier

--- header.h
+++ header.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 #include <vector>
+#include <map>
 #include <iostream>
 #include <fstream>
 
@@ -8,7 +9,170 @@
 
 ///Proto
 void menu();
+void Chargement_Graphe();
 
+
+
+
+
+class Sommet
+{
+
+private :
+    int m_num;
+    std::string m_nom;
+    int m_x,m_y;
+    ///chaque sommet possède la liste de ses successeurs (un vecteur de pointeurs sur Sommet)
+    std::map<const Sommet*,int> m_successeurs;
+
+public :
+    /*constructeur*/
+    Sommet(int num,std::string nom,int x,int y):m_num{num},m_nom{nom},m_x{x},m_y{y} {};
+
+    /*accesseurs*/
+    int getNum()const
+    {
+        return m_num;
+    }
+
+    ///accesseur : pour la liste des successeurs
+    const std::map<const Sommet*,int>& getSuccesseurs()const
+    {
+        return m_successeurs;
+    }
+
+    /* pour ajouter un successeur à la liste*/
+   void ajouterSucc(const Sommet*s,int poids)
+    {
+        m_successeurs[s]=poids;
+    }
+
+
+    /* méthode d'affichage*/
+    void afficher() const
+    {
+        std::cout<<"     sommet "<<m_nom<<"-"<<m_num<<" Coords : "<<m_x<<" ; "<<m_y<<" : ";
+        for (auto s : m_successeurs)
+            std::cout<<s.first->getNum()<<" ("<<s.second<<") ";
+    }
+
+
+
+};
+
+
+
+class Graphe
+{
+
+private :
+
+    ///liste des sommets (vecteur de pointeurs sur Sommet)
+    std::vector<Sommet*> m_sommets;
+    int m_orientation;
+
+public :
+    /* La construction du réseau peut se faire à partir d'un fichier
+     dont le nom est passé en paramètre
+    */
+
+    Graphe(std::string fichier_topo,std::string fichier_ponde )
+    {
+
+        std::ifstream ifs{fichier_topo};
+        std::ifstream ifp{fichier_ponde};
+
+        if (!ifs)
+            throw std::runtime_error( "Impossible d'ouvrir en lecture " + fichier_topo );
+
+        ifs >> m_orientation;
+
+        if ( ifs.fail() )
+            throw std::runtime_error("Probleme lecture orientation du graphe");
+
+        int ordre;
+        ifs >> ordre;
+        std::cout<<ordre;
+        if ( ifs.fail() )
+            throw std::runtime_error("Probleme lecture ordre du graphe");
+
+        for(int i=0; i<ordre; i++)
+        {
+
+            int index;
+            ifs >> index;
+            if ( ifs.fail() )
+                throw std::runtime_error("Probleme lecture de l'index du sommet");
+
+                 std::string nom ;
+            ifs >> nom ;
+            if ( ifs.fail() )
+                throw std::runtime_error("Probleme lecture du nom du sommet");
+
+            int x,y;
+            ifs >> x>>y;
+            if ( ifs.fail() )
+                throw std::runtime_error("Probleme lecture des coords du sommet");
+
+
+            m_sommets.push_back( new Sommet{index,nom,x,y});
+
+
+        }
+
+        int taille;
+        ifs >> taille;
+        if ( ifs.fail() )
+            throw std::runtime_error("Probleme lecture taille du graphe");
+
+        int num1,num2,index,poids;
+
+        for (int i=0; i<taille; ++i)
+        {
+            ifs>>index>>num1>>num2;
+            ifp>>poids;
+
+            if ( ifs.fail() )
+                throw std::runtime_error("Probleme lecture arc");
+
+
+            m_sommets[num1]->ajouterSucc(m_sommets[num2],poids);
+            ///si le graphe n'est pas orienté
+            ///si num2 est successeur de num1, num1 est successeur de num2
+            if(!m_orientation)
+                m_sommets[num2]->ajouterSucc(m_sommets[num1],poids);
+
+        }
+
+
+    }
+
+    /*destructeur*/
+    ~Graphe()
+    {
+        for (auto s : m_sommets)
+            delete s;
+    }
+
+    /*méthode d'affichage*/
+    void afficher() const
+    {
+        std::cout<<std::endl<<"graphe ";
+        if(m_orientation)
+            std::cout<<"oriente"<<std::endl<<"  ";
+        else
+            std::cout<<"non oriente"<<std::endl<<"  ";
+        std::cout<<"ordre = "<<m_sommets.size()<<std::endl<<"  ";
+        std::cout<<"listes d'adjacence :"<<std::endl;
+        for (auto s : m_sommets)
+        {
+            s->afficher();
+            std::cout<<std::endl;
+        }
+    }
+
+
+};
 
 
 
--- main.cpp
+++ main.cpp
@@ -7,53 +7,59 @@ int main()
 {
     cout << "--Projet Theorie des graphes-- \n" << endl;
     while(!0)
-    menu();
+        menu();
 
     return 0;
 }
 
 void menu()
 {
-int choix ;
-std::string fichier_topo;
-std::string fichier_ponde;
+    int choix ;
 
-std::cout<<"            Menu \n\n";
 
-std::cout<<"1/Chargement d'un Graphe. \n";
-std::cout<<"2/Chagement du systeme de ponderation. \n";
-std::cout<<"3/Calculer, afficher et Sauvegarder les differents indices de centralite.\n";
-std::cout<<"4/Tester la vulnerabilite. \n";
-std::cout<<"99/Exit\n\n ";
-std::cout<<"Votre choix : " ;
-std::cin>>choix ;
-std::cout<<"\n";
+    std::cout<<"            Menu \n\n";
 
-switch (choix)
+    std::cout<<"1/Chargement d'un Graphe. \n";
+    std::cout<<"2/Chagement du systeme de ponderation. \n";
+    std::cout<<"3/Calculer, afficher et Sauvegarder les differents indices de centralite.\n";
+    std::cout<<"4/Tester la vulnerabilite. \n";
+    std::cout<<"99/Exit\n\n ";
+    std::cout<<"Votre choix : " ;
+    std::cin>>choix ;
+    std::cout<<"\n";
+
+    switch (choix)
+    {
+
+    case 1 :
+
+        Chargement_Graphe();
+        break;
+
+    case 99 :
+
+        exit(0);
+        break;
+
+    }
+
+
+
+}
+
+void Chargement_Graphe()
 {
-
-case 1 :
-
-    std::cout<<"- Chargement d'un Graphe.\n";
+    std::string fichier_topo;
+    std::string fichier_ponde;
 
     std::cout<<"Nom du fichier de topologie : ";
     std::cin>>fichier_topo;
     std::cout<<"Nom du fichier de ponderations : ";
     std::cin>>fichier_ponde;
-
     std::cout<<"\n Chargement du graphe avec "<<fichier_topo << "et" <<fichier_ponde<< " \n\n" ;
 
-break;
 
-case 99 :
-    exit(0);
-
-
-default :
-    std::cout<<"Erreur \n";
-
-}
-
-
+    Graphe A { fichier_topo,fichier_ponde };
+    A.afficher();
 
 }
